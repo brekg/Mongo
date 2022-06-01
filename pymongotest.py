e95dea4d06ef6a080c1d11126a1201d963ee556d
Brekg <brek.graham@gmail.com>
2022-06-01 17:33:38 -0500

    Not sure why

--- pymongotest.py
+++ pymongotest.py
@@ -1,6 +1,22 @@
 import pymongo
+import json
+import pprint
 
-atlas_cluster = "mongodb+srv://m001-student:m001-mongodb-basics@sandbox.zlntu.mongodb.net/sample_airbnb?authSource=admin&replicaSet=atlas-mqtc1h-shard-0&readPreference=primary"
-client = pymongo.MongoClient(atlas_cluster)
-db = client.sample_airbnb
-print(db.name)
+try:
+
+    atlas_cluster = "mongodb+srv://m001-student:m001-mongodb-basics@sandbox.zlntu.mongodb.net/sample_airbnb?authSource=admin&replicaSet=atlas-mqtc1h-shard-0&readPreference=primary"
+    client = pymongo.MongoClient(atlas_cluster)
+    db = client.sample_airbnb
+
+    query =  {"bedrooms":{"$gt":1}}
+    cursor = db.listingsAndReviews.find(query)
+    list_cur = list(cursor)
+    json_data = json.dumps(list_cur, indent = 2)
+    pprint(json_data.count)
+
+except Exception as e:
+        print("An exception occurred ::", e)
+
+finally:
+    print("*************** EXITING ****************")
+ 
