f2b95d06330b08cc26cf48e05aad80b57a0f0dc5
Brekg <brek.graham@gmail.com>
2022-07-09 17:55:24 -0500

    This is prior to changing the from pipeline to findOne()

--- pyaggr_geo.py
+++ pyaggr_geo.py
@@ -1,7 +1,9 @@
 from pymongo import MongoClient
+from pandas import DataFrame
 import pprint
 import json 
 
+#https://stackoverflow.com/questions/63162823/mongo-geowithin-error-polygon-coordinates-must-be-an-array
 def main():
     # Call one of the outside functions
     try:    
@@ -12,14 +14,25 @@ def main():
 
         pipeline_county = [
             {'$match': {'name':'Jefferson'}},
-            {'$project': {'_id': 0, 'uuid': 1, 'name': 1, 'location': 1}},
+            {'$project': {'_id': 0, 'uuid': 1, 'name': 1, 'location.coordinates': 1}},
         ]
 
         results = database.counties.aggregate(pipeline_county)
         
         list_cur = list(results)
         json_data = json.dumps(list_cur, indent = 2)
-        #pprint(json_data)
+        polygon = db.polygons.findOne()    
+
+        db.data.find({
+            location: {
+                $geoWithin: {
+                    $geometry: {
+                        type: 'Polygon',
+                        coordinates: list_cur.location.coordinates
+                    }
+                }
+            }
+        }).count()
 
     except Exception as e:
         print("An exception occurred ::", e)
